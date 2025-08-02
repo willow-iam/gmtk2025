bff41f122293038c347e86275e1e46d4b1fd7125
willregex <willowhelmiam@gmail.com>
2025-08-02 16:03:01 -0400

    Easiest Interactables

--- GameClock.gd
+++ GameClock.gd
@@ -10,9 +10,13 @@ var time:
 			return max_time*60
 
 func _ready() -> void:
+	restart()
+
+func restart():
 	timer = get_child(0)
 	timer.start(max_time*60)
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
+	timer.paused = Dialogic.current_timeline!=null
 	text = "11:%02d:%02d"%[(60-max_time+int(time/60)), (time%60)]
 	
--- player.gd
+++ player.gd
@@ -7,13 +7,14 @@ var interact_range : ShapeCast2D
 func _ready():
 	interact_range = get_node("InteractRange") as ShapeCast2D
 	interact_range.add_exception(self)
-	GameClock
+	GameClock.restart()
+	Dialogic.start("Intro")
 
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	if not Dialogic.current_timeline:
 		get_input()
 		move_and_slide()
@@ -23,4 +24,9 @@ func _physics_process(delta):
 				var that = interact_range.get_collider(i)
 				if that is Interactable and Input.is_action_pressed("interact"):
 					Dialogic.start(that.timeline)
+					#Dialogic.VAR.set_variable("spoken.%s"%(that.name as String), true)
+					Dialogic.timeline_ended.connect(
+						func():
+							Dialogic.VAR.set_variable("spoken.%s"%(that.name as String), true)
+					)
 					return
